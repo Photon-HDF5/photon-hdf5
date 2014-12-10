d8b298c3f28c2bc1b496375d997fb649b83a687d
Antonino Ingargiola <tritemio@gmail.com>
2014-12-09 23:45:35 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Photon-HDF5'
-copyright = u'2014, Antonino Ingargiola, Xavier Michalet'
+copyright = u'2014, Antonino Ingargiola, Xavier Michalet, Ted Laurence'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
