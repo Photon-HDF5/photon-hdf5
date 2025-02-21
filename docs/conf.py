47416eea9587f28919ba44fc857575b78ed11dd0
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-02-21 12:25:01 -0600

    Remove html_theme_path setting
    Running the sphinx build gave the following warning, so do what it says.
    
    > WARNING: Calling get_html_theme_path is deprecated. If you are calling it to define html_theme_path, you are safe to remove that code.
    
    Also remove the surrounding code that becomes unnecessary.
    
    This change seems to have fixed the search box.

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,7 @@
 import sys
 import os
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    #html_theme_path = ["_themes", ]
-    html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
